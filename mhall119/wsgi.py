e0cf6a0cbf8211cfc59b8c7ce81dd07568a34c98
Michael Hall <mhall119@gmail.com>
2018-06-09 12:51:57 -0400

    Add django project to pythonpath

--- mhall119/wsgi.py
+++ mhall119/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("mhall119"))
